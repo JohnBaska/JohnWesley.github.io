78a5e5d23d4656c899bd0d0349398e694040d8f3
john.diniz <john.diniz@inventvision.com.br>
2024-08-30 17:09:34 -0300

    Primeiro projeto colocado

--- Principal.py
+++ Principal.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+class Portfolio:
+    def __init__ (self, import_name):
+        self.app = Flask(__name__)
+        self.app.secret_key = os.urandom(24)
+        self.paginaPrincipal()
+        self.paginasProjetos()
+    
+    def paginaPrincipal(self):
+        @self.app.route('/')
+        def home():
+            return render_template('index.html')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    def paginasProjetos(self):
+        @self.app.route('/Site-Curriculo')
+        def siteCurriculo():
+            return render_template('Site-Curriculo.html')
+    
+    def run(self, **kwargs):
+        # Método para rodar a aplicação
+        self.app.run(**kwargs)
 
 if __name__ == '__main__':
+    app = Portfolio(__name__)
     app.run(debug=True)
